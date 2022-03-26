71d92ff78272fa796c6af6cb0bbd3386b101b14f
Rumana K <rumana2k20@gmail.com>
2022-03-26 17:07:12 +0600

    Showed item name on cart

--- src/components/Ring/Ring.js
+++ src/components/Ring/Ring.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './Ring.css'
 
-const Ring = (props) => {
-    const {name, price, picture, } = props.ring
+const Ring = ({cartButtonClick, ring }) => {
+    const {name, price, picture, id } = ring
+
     return (
         <div className='ring'>
             <img src={picture} alt="" />
             <h2>{name}</h2>
             <p>price: {price}</p>
-            <button className='cart-btn'>ADD TO CART</button>
+            <button className='cart-btn' onClick={()=>cartButtonClick({ring})} >ADD TO CART</button>
         </div>
     );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -4,21 +4,40 @@ import './Shop.css'
 
 const Shop = () => {
     const [rings, setRings] = useState([]);
+    const [cart, setCart] = useState([])
+
     useEffect(() => {
         fetch('products.json')
             .then(res => res.json())
             .then(data => setRings(data))
     }, [])
+
+    const cartButtonClick = (ring) => {
+        const newCart = [...cart, [ring]];
+        setCart(newCart)
+
+    }
     return (
         <div className="shopContainer">
             <div className='shop'>
-                {rings.map(ring => <Ring key={ring.id} ring={ring} ></Ring>)}
+                {rings.map(ring => <Ring
+                    key={ring.id}
+                    ring={ring}
+                    cartButtonClick={cartButtonClick}
+
+
+                ></Ring>)}
             </div>
             <div className='selectItem'>
-                <h3>Select Rings</h3>
+                <h3>Select Rings: {cart.length}</h3>
+                {cart.map((item) => (
+                    <h2>{item[0].ring.name}</h2> 
+                ))}
+
                 <button className='select-btn'>CHOOSE ONE</button>
                 <button className='select-btn'>CHOOSE AGAIN</button>
                 
+
             </div>
         </div>
 
